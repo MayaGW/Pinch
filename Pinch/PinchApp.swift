bf574632b91274a8dc2be5ac33dfbdc800ab5477
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-02-13 15:54:48 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Maya Ghamloush on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
